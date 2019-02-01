36bbf471168e312cf7d164a2e65ab45abc156436
Neil Girardi <neil.girardi@gmail.com>
2019-01-31 19:28:58 -0500

    Move API key to .env file.

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  API_URL: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json',
-  API_KEY: 'A1GXD8rMOCiIBUxVA39okYAFxefAWw8y'
+  API_URL: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json'
 }
--- src/backend/routes/reviews.js
+++ src/backend/routes/reviews.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 
 const router = express.Router();
 const config = require('./../config')
+const API_KEY = process.env.API_KEY
+console.log('API_KEY: ', process.env.API_KEY)
 
 router.get('/:query', function(req, res, next) {
   const { params } = req
   const { query } = params
-  const url = `${config.API_URL}?query=${query}&api-key=${config.API_KEY}`
+  const url = `${config.API_URL}?query=${query}&api-key=${API_KEY}`
   axios.get(url)
     .then(response => {
       const { data } = response
